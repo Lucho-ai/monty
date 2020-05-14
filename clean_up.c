9f0e19bec69760c46f281cb4460ba84839df07b9
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-14 10:44:30 -0500

    Create clean_up.c

---
+++ clean_up.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * clean_up - Frees malloc objects and closes the file
+ * @stack: Pointer to the stack
+ * Return: Nothing
+ */
+void cleanup(stack_t **stack)
+{
+	stack_t *ptr = *stack;
+
+	for (; ptr; ptr = *stack)
+	{
+		*stack = (*stack)->next;
+		free(ptr);
+	}
+	fclose(mem.fs);
+	free(mem.line);
+}
