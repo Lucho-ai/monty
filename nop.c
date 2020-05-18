3d3f3886c10785fb7386ffcf1f38474b04cb39fd
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-18 12:05:13 -0500

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "monty.h"
+/**
+ * nop - The opcode that doesn’t do anything.
+ * @h: The stack pointer..
+ * @count: counter of the line
+ * Return: Void.
+ */
+void nop(stack_t **h, unsigned int count)
+{
+	(void) h;
+	(void) count;
+}
