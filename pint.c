8fcbc3fc54ebd63a9175952a522e4691a0f489f4
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-18 12:07:25 -0500

    Create pint.c

---
+++ pint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "monty.h"
+/**
+ * pint - function that prints the last element of a list
+ * @h : const - list_t  
+ * @count: count.
+ * Return: the number of notes.
+ */
+void pint(stack_t **h, unsigned int count)
+{
+	stack_t *temp;
+
+	temp = *h;
+	if (temp == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: can't pint, stack empty\n", count);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", temp->n);
+}
