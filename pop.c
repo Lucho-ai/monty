bce89b98280c3ef8a1475673885a7e3ca29882f8
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-18 12:08:51 -0500

    Create pop.c

---
+++ pop.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * pop - Funtionces of the opcode pop.
+ * @h: Stack.
+ * @count: count of the line where the opcode is taken.
+ * Return: Void.
+ */
+void pop(stack_t **h, unsigned int count)
+{
+	stack_t *temp;
+
+	temp = *h;
+	if (temp == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", count);
+		exit(EXIT_FAILURE);
+	}
+	*h = temp->next;
+	if (temp->next)
+		temp->next->prev = NULL;
+	free(temp);
+}
