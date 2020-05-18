ce7c30d47844cdd95160b64854b8e84e56261100
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-18 12:37:35 -0500

    Create analityc.c

---
+++ analityc.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * analityc - Parser to the buffer the comes from opcode-file.
+ * @buffer: The buffer with the imformation.
+ * @h: The stack
+ * @count: Count of the line where the opcode is taken.
+ * Return: Void.
+ */
+
+void analityc(char *buffer, stack_t **h, unsigned int count)
+{
+	char *str, *token;
+
+	str = strtok(buffer, " \n\t");
+	token = strtok(NULL, " \n\t");
+	sup.num = token;
+	if (str && str[0] != '#')
+		get_op_func(h, count, str);
+}
