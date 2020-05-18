93051bd60f710a146bf234d4b1faec67ccaabe52
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-18 12:19:25 -0500

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * swap - Funtion that swap the last valeus.
+ * @h: Stack.
+ * @count: count of the line where the opcode is taken.
+ * Return: Void
+ */
+void swap(stack_t **h, unsigned int count)
+{
+	stack_t *two;
+	int i = 0, temp;
+
+	two = (*h);
+
+	while (two)
+	{
+		i++;
+		two = two->next;
+	}
+	if (i < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*h)->n;
+	(*h)->n = (*h)->next->n;
+	(*h)->next->n = temp;
+}
