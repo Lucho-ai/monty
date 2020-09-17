02a8badb3c16dc7a3df98fb334e304113c00a865
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-09-17 16:27:03 -0500

    Update up_func.c

--- up_func.c
+++ up_func.c
@@ -1,26 +1,26 @@
 #include "monty.h"
 /**
- * up_func - Funtion.
+ * get_op_func - Funtion.
  * @command: command.
  * @h: h.
  * @count: count.
  */
-void up_func(stack_t **h, unsigned int count, char *command)
+void get_op_func(stack_t **h, unsigned int count, char *command)
 {
 	int i = 0;
 
 	instruction_t op[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pop", pop},
-		{"pint", pint},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{"sub", sub},
-		{"div", div},
-		{"mul", mul},
-		{"mod", mod},
+		{"push", _push},
+		{"pall", _pall},
+		{"pop", _pop},
+		{"pint", _pint},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
 		{NULL, NULL}
 	};
 
