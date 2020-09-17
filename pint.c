3aa39ceac964c32d163e1182eeba8f0aa318b974
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-09-17 16:22:30 -0500

    Update pint.c

--- pint.c
+++ pint.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include "monty.h"
 /**
- * pint - function that prints the last element of a list
+ * _pint - function that prints the last element of a list
  * @h : const - list_t
  * @count: count.
  * Return: the number of notes.
  */
-void pint(stack_t **h, unsigned int count)
+void _pint(stack_t **h, unsigned int count)
 {
 	stack_t *temp;
 
