db9ca38428edfd27270ee1576b1b39fb13af2d74
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-09-17 16:21:58 -0500

    Update pall.c

--- pall.c
+++ pall.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include "monty.h"
 /**
- *  pall - function that prints all the elements of a dlist
+ *  _pall - function that prints all the elements of a dlist
  * @h : const - list_t
  * @count: count.
  * Return: the number of notes.
  */
-void pall(stack_t **h, unsigned int count)
+void _pall(stack_t **h, unsigned int count)
 {
 	stack_t *temp;
 
